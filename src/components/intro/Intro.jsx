b05fb64e222503fc5e178571773213d0b977c0a7
arumbaua366 <arumbaua366@gmail.com>
2023-02-07 00:14:00 -0800

    updated intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const intro = () => {
-  return <div>this is the intro</div>;
+const Intro = () => {
+  return (
+    <div className="i">
+      <div className="i-left">left</div>
+      <div className="i-right">right</div>
+    </div>
+  );
 };
 
-export default intro;
+export default Intro;
