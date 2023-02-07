5f60867f6caa4c47184448769fe54dd73037aaf8
arumbaua366 <arumbaua366@gmail.com>
2023-02-07 00:32:51 -0800

    added name and initial css to intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import "./intro.css";
 
 const Intro = () => {
   return (
     <div className="i">
-      <div className="i-left">left</div>
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, Ny name is</h2>
+          <h1 className="i-name">Andrea Rumbaua</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item"></div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="i-right">right</div>
     </div>
   );
