21a22d43564c153e9bf9cde54d46e0df9e32408d
arumbaua366 <arumbaua366@gmail.com>
2023-02-13 23:33:37 -0800

    create productList and prodct components

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,18 @@
+import "./productList.css";
+
+const ProductList = () => {
+  return (
+    <div className="p1">
+      <div className="p1-texts">
+        <h1 className="pl-title">Create & inspire. It's Lama</h1>
+        <p className="pl-desc">
+          Lama is a creative portfolio that your work has been waiting for.
+          Beautiful homes, stunning portfolio styles & a whole lot more awaits
+          inside.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
