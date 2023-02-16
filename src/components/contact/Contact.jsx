e85f5e9199356adf051e2c187ecde56a4d59ff59
arumbaua366 <arumbaua366@gmail.com>
2023-02-16 00:47:58 -0800

    create contact component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,35 @@
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+
+const Contact = () => {
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">
+            Feel free to reach out to me through these platforms.
+          </h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />1 (123) 123-1234
+            </div>
+            <div className="c-info-item">
+              <img src={Email} alt="" className="c-icon" />
+              andrea.rumbaua@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img src={Address} alt="" className="c-icon" />
+              San Jose, CA
+            </div>
+          </div>
+        </div>
+        <div className="c-right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
