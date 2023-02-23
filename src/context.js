045640d2c143f2c7910013d7ed6316ef5ec2c913
arumbaua366 <arumbaua366@gmail.com>
2023-02-22 23:38:23 -0800

    update darkMode button to swith from light to dark mode on click

--- src/context.js
+++ src/context.js
@@ -13,6 +13,7 @@ const themeReducer = (state, action) => {
   }
 };
 
+// ThemeProvider provides the "theme" to the components of your choice. E.g. if you only want to darkMode used in the Intro and About pages but not ProductList or Contact. Can be added to app.jsx or for the entire app, you can add directly to index.js
 export const ThemeProvider = (props) => {
   const [state, dispatch] = useReducer(themeReducer, INITIAL_STATE);
 
